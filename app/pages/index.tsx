a9b5ba5ce3d7db133fd7652fae7cd9347cab2d5a
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-07 12:11:06 +0100

    Strips index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react"
 import { Image, Link, BlitzPage, useMutation, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import logo from "public/logo.png"
 import { Navbar } from "app/core/components/navbar"
 
 /*
@@ -14,10 +13,6 @@ const Home: BlitzPage = () => {
     <div className="container">
       <main>
         <Navbar />
-
-        <div className="logo">
-          <Image src={logo} alt="blitzjs" />
-        </div>
         <p>
           <strong>Congrats!</strong> Your app is ready, including user sign-up and log-in.
         </p>
@@ -25,59 +20,6 @@ const Home: BlitzPage = () => {
           <Suspense fallback="Loading...">
             <p>useCurrentUser was here</p>
           </Suspense>
-        </div>
-        <p>
-          <strong>
-            To add a new model to your app, <br />
-            run the following in your terminal:
-          </strong>
-        </p>
-        <pre>
-          <code>blitz generate all project name:string</code>
-        </pre>
-        <div>(And select Yes to run prisma migrate)</div>
-        <div>
-          <p>
-            Then <strong>restart the server</strong>
-          </p>
-          <pre>
-            <code>Ctrl + c</code>
-          </pre>
-          <pre>
-            <code>blitz dev</code>
-          </pre>
-          <p>
-            and go to{" "}
-            <Link href="/projects">
-              <a>/projects</a>
-            </Link>
-          </p>
-        </div>
-        <div className="buttons">
-          <a
-            className="button"
-            href="https://blitzjs.com/docs/getting-started?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-          <a
-            className="button-outline"
-            href="https://github.com/blitz-js/blitz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github Repo
-          </a>
-          <a
-            className="button-outline"
-            href="https://discord.blitzjs.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Discord Community
-          </a>
         </div>
       </main>
 
