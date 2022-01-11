699115a47a846962243036a55b4e10e888be8dba
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-11 12:59:07 +0100

    Remove blitz comment from index

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -5,11 +5,6 @@ import { Hero } from "@/components/Hero"
 import { BlockChains } from "@/components/BlockChains"
 import { Sponsors } from "@/components/Sponsors"
 
-/*
- * This file is just for a pleasant getting started page for your new app.
- * You can delete everything in here and start from scratch if you like.
- */
-
 const Home: BlitzPage = () => {
   return (
     <>
