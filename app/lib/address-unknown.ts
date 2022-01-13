01563521718881833251a724f1313795e8f4d575
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-13 18:17:45 +0100

    Coding standards

--- app/lib/address-unknown.ts
+++ app/lib/address-unknown.ts
@@ -3,7 +3,6 @@ import { BlockchainAddress } from "./blockhain-address"
 /**
  * Catch all class for all unrecognizable addresses.
  */
-
 export class UnknownAddress extends BlockchainAddress {
   constructor(address: string) {
     super(address) // sets address property in the base class (lowercased there)
