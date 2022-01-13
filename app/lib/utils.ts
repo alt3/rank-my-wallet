b2a4457c42417307608efa66f98c6df5837d82bb
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-13 16:57:44 +0100

    Adds getRandomInt() to utils lib

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -7,3 +7,15 @@
 export const byteToBits = function (number: number, bits: number): Array<number> {
   return [...Array(bits)].map((x, i) => (number >> i) & 1)
 }
+
+/**
+ * Return an pseudo random number.
+ *
+ * @param min - Lowest possible number, must be be higher or equal
+ * @param min - Highest possible number, must be lower or equal
+ */
+export const getRandomInt = function (min: number, max: number): number {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
