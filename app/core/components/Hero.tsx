246fb714f7011f39e0543fbee0645894cb165d36
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-15 17:28:35 +0100

    Cleanup Hero

--- app/core/components/Hero.tsx
+++ app/core/components/Hero.tsx
@@ -1,15 +1,5 @@
 import React from "react"
-import {
-  chakra,
-  useColorModeValue,
-  Box,
-  Center,
-  Container,
-  Stack,
-  HStack,
-  Input,
-  Button,
-} from "@chakra-ui/react"
+import { Heading, useColorModeValue, Box, Center, Container, Stack } from "@chakra-ui/react"
 import { AddressForm } from "@/components/AddressForm"
 
 export function Hero() {
@@ -21,14 +11,10 @@ export function Hero() {
     >
       <Container maxW="container.md" paddingBottom={{ base: "3.5rem", md: "5rem" }}>
         <Center>
-          <Stack
-            width="100%"
-            spacing={8}
-            fontSize={{ base: "4xl", lg: "6xl", sm: "4xl" }}
-            fontWeight="bold"
-            align="center"
-          >
-            <chakra.h2>Rank my wallet:</chakra.h2>
+          <Stack width="100%" spacing={8} align="center">
+            <Heading fontSize={{ base: "4xl", lg: "6xl", sm: "4xl" }} fontWeight="bold">
+              Rank my wallet:
+            </Heading>
             <AddressForm />
           </Stack>
         </Center>
