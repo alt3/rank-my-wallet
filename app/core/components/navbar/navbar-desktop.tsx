2152ff55caf9c139d07881fe2742742071e0ed8f
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-19 18:12:39 +0100

    Fix navbar

--- app/core/components/navbar/navbar-desktop.tsx
+++ app/core/components/navbar/navbar-desktop.tsx
@@ -1,35 +1,61 @@
 import { Link } from "@/components/Link"
-import { MoonIcon, SunIcon } from "@chakra-ui/icons"
-import {
-  Box,
-  Container,
-  HStack,
-  Spacer,
-  Stack,
-  Switch,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react"
+import { Box, Flex, Icon, IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react"
+import { BsMoonFill, BsSunFill, BsTwitter } from "react-icons/bs"
 
 export const NavbarDesktop: React.FC = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { toggleColorMode: toggleMode } = useColorMode()
+  const text = useColorModeValue("dark", "light")
+  const SwitchIcon = useColorModeValue(BsMoonFill, BsSunFill)
 
   return (
-    <Box color={useColorModeValue("gray.600", "whiteAlpha.600")}>
-      <Container as={Stack} d={{ base: "flex", md: "flex" }} maxW="5xl" py={4} spacing={4}>
-        <HStack>
+    <Box
+      color={useColorModeValue("gray.700", "whiteAlpha.600")}
+      fontWeight={useColorModeValue(600, 500)}
+      maxW="5xl"
+      mx="auto"
+      paddingTop={2}
+    >
+      <Flex w="100%" h="100%" px="6" align="center" justify="space-between">
+        <Flex align="center">
           <Link href="/">Home</Link>
+        </Flex>
 
-          <Spacer />
-
-          <Link href="/faq" paddingRight="1rem">
-            FAQ
+        <Flex
+          justify="flex-end"
+          w="100%"
+          align="center"
+          maxW="1100px"
+          color={useColorModeValue("gray.700", "whiteAlpha.600")}
+        >
+          <Link
+            href="https://www.twitter.com/RankMyWallet/"
+            aria-label="Visit RankMyWallet on Twitter"
+            isExternal
+            passHref
+          >
+            <Icon
+              as={BsTwitter}
+              w={5}
+              h={5}
+              marginRight="0.25rem"
+              marginTop="0.35rem"
+              _hover={{ color: "#1D9AEF" }}
+            />
           </Link>
-          <SunIcon />
-          <Switch isChecked={colorMode === "dark"} onChange={() => toggleColorMode()} />
-          <MoonIcon />
-        </HStack>
-      </Container>
+
+          <IconButton
+            size="lg"
+            maxWidth="1rem"
+            ml={0}
+            aria-label={`Switch to ${text} mode`}
+            variant="ghost"
+            color="current"
+            marginLeft="0"
+            onClick={toggleMode}
+            icon={<SwitchIcon />}
+          />
+        </Flex>
+      </Flex>
     </Box>
   )
 }
