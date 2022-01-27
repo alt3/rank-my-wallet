212005a4edc33c2746a76c1e1f1fa076c57438d2
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-27 23:40:52 +0100

    Adds the ErrorHero component

---
+++ app/core/components/ErrorHero/ErrorHero.tsx
@@ -0,0 +1,46 @@
+import { Box, Center, Container, Heading, Stack, useColorModeValue } from "@chakra-ui/react"
+
+interface ErrorHeroProps {
+  title: string
+  subtitle?: string
+}
+
+export const ErrorHero = ({ title, subtitle }: ErrorHeroProps) => {
+  const styles = {
+    box: {
+      marginBottom: "3rem",
+    },
+    container: {
+      maxW: "container.md",
+      marginTop: { base: "3rem", md: "4rem" },
+      // paddingBottom: { base: "4rem", md: "5rem" },
+    },
+    title: {
+      fontSize: { base: "3xl", md: "6xl" },
+      fontWeight: "bold",
+      letterSpacing: "tighter",
+      color: useColorModeValue("teal.500", "teal.300"),
+      marginBottom: 0,
+      paddingBottom: 0,
+    },
+    subtitle: {
+      color: useColorModeValue("gray.700", "whiteAlpha.900"),
+      textAlign: "center" as const,
+    },
+  }
+
+  return (
+    <Box {...styles.box}>
+      <Container {...styles.container}>
+        <Center>
+          <Stack width="100%" spacing={8} align="center">
+            <Heading {...styles.title}>{title}</Heading>
+            {subtitle && <Heading {...styles.subtitle}>{subtitle}</Heading>}
+          </Stack>
+        </Center>
+      </Container>
+    </Box>
+  )
+}
+
+export default ErrorHero
