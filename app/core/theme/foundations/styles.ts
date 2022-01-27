4272476a1f825db8f07a10fbd51eb92407b9e107
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-27 20:01:25 +0100

    Remove bold for light mode to prevent breaking footer on iOS SE

--- app/core/theme/foundations/styles.ts
+++ app/core/theme/foundations/styles.ts
@@ -3,7 +3,6 @@ export const styles = {
     html: {},
     body: {
       backgroundColor: props.colorMode === "light" ? "light.900" : "gray.800",
-      fontWeight: props.colorMode === "light" ? "bolder" : "normal",
       color: props.colorMode === "light" ? "gray.700" : "whiteAlpha.900",
     },
   }),
