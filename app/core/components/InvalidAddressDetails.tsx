42d2609b88f5c2309054d56ec6c72801e2abcaeb
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-01-28 11:55:09 +0100

    Adds address analysis to the error pages

--- app/core/components/InvalidAddressDetails.tsx
+++ app/core/components/InvalidAddressDetails.tsx
@@ -6,6 +6,7 @@ import {
   AccordionPanel,
   Box,
   Container,
+  Divider,
   Grid,
   GridItem,
   Text,
@@ -24,11 +25,126 @@ import { capitalize, getNextSpecies, getSpecies } from "app/lib/utils"
 import nextId from "react-id-generator"
 
 export function InvalidAddressDetails({ parsedAddress }) {
+  console.log(parsedAddress)
+  const colorTeal = useColorModeValue("teal.500", "teal.300")
+
   return (
     <Container maxW="container.md" marginBottom="2.5rem">
       <ErrorHero title="Address Error" subtitle={parsedAddress.error.message}></ErrorHero>
 
-      <AddressForm placeholder="Try again?" marginTop={{ base: "3rem", sm: "5rem" }} />
+      <AddressForm
+        placeholder="Try again?"
+        marginTop={{ base: "3rem", sm: "5rem" }}
+        marginBottom={{ base: "4rem", sm: "7rem" }}
+      />
+
+      {parsedAddress.blockchain === "cardano" ||
+        (parsedAddress.blockchain === "ergo" && (
+          <>
+            <Divider marginBottom={{ base: "1.5rem", sm: "1.5rem" }} />
+
+            <Accordion allowMultiple>
+              <AccordionItem borderStyle="none" marginBottom={{ base: "1rem", sm: "0.5rem" }}>
+                <h2>
+                  <AccordionButton p={0}>
+                    <Box flex="1" textAlign="left">
+                      <SectionHeader>Address Details</SectionHeader>
+                    </Box>
+                    <Box as="span" verticalAlign="top" minHeight="3rem">
+                      <AccordionIcon color={colorTeal} />
+                    </Box>
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel p={0} pb="2rem">
+                  <DataGrid marginBottom={{ base: "1.5rem", sm: "1.5rem" }}>
+                    <DataGridEntry field="Address" value={parsedAddress.address} />
+                    <DataGridEntry
+                      field="Blockchain"
+                      value={`${capitalize(parsedAddress.blockchain)} (${parsedAddress.ticker})`}
+                    />
+                    {parsedAddress.version && (
+                      <DataGridEntry
+                        field="Address Version"
+                        value={capitalize(parsedAddress.version)}
+                      />
+                    )}
+                    {parsedAddress.network && (
+                      <DataGridEntry field="Network" value={capitalize(parsedAddress.network)} />
+                    )}
+                    {parsedAddress.type && (
+                      <DataGridEntry
+                        field="Address Type"
+                        value={capitalize(parsedAddress.type.name)}
+                      />
+                    )}
+                  </DataGrid>
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem borderStyle="none">
+                <AccordionButton p={0}>
+                  <Box flex="1" textAlign="left">
+                    <SectionHeader>Address Analysis</SectionHeader>
+                  </Box>
+                  <Box as="span" verticalAlign="top" minHeight="3rem">
+                    <AccordionIcon color={colorTeal} />
+                  </Box>
+                </AccordionButton>
+
+                <AccordionPanel p={0} pb={4}>
+                  <DataGrid>
+                    <DataGridEntry field="Encoding" value={capitalize(parsedAddress.encoding)} />
+                    {parsedAddress.decoded !== undefined &&
+                      parsedAddress.decoded.prefix !== undefined && (
+                        <DataGridEntry
+                          field="Decoded Prefix"
+                          value={parsedAddress.decoded.prefix}
+                        />
+                      )}
+
+                    {parsedAddress.header !== undefined && (
+                      <DataGridEntry field="Header Byte" value={parsedAddress.header.byte} />
+                    )}
+                  </DataGrid>
+
+                  <Text color={colorTeal} marginBottom="0.25rem">
+                    Decoded Bytes:
+                  </Text>
+                  <Grid
+                    templateColumns={{ base: "repeat(4, 1fr)", sm: "repeat(6, 1fr)" }}
+                    paddingBottom="1.5rem"
+                  >
+                    {parsedAddress.bytes.map((byte) => (
+                      <GridItem key={nextId("byte")}>{byte}</GridItem>
+                    ))}
+                  </Grid>
+
+                  {parsedAddress.header !== undefined && (
+                    <>
+                      <BitsTable
+                        caption="Header Bits:"
+                        bits={parsedAddress.header.bits}
+                        sumLabel="Header Byte"
+                      ></BitsTable>
+
+                      <BitsTable
+                        caption="Leading Header Bits:"
+                        bits={parsedAddress.header.leading.bits}
+                        sumLabel={parsedAddress.header.leading.type}
+                      ></BitsTable>
+
+                      <BitsTable
+                        caption="Trailing Header Bits:"
+                        bits={parsedAddress.header.trailing.bits}
+                        sumLabel={parsedAddress.header.trailing.type}
+                      ></BitsTable>
+                    </>
+                  )}
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </>
+        ))}
     </Container>
   )
 }
