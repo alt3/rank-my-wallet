5d963a6ce2a6888a772d6bac0cb615b02d7bc069
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-02-14 17:55:31 +0100

    Adds coming soon for Cardano rankings

--- app/core/components/Counter.tsx
+++ app/core/components/Counter.tsx
@@ -44,18 +44,31 @@ export function Counter({ totalAccounts, rank, blockchain }: CounterProps) {
       <Container {...styles.container}>
         <Center>
           <Stack spacing={2} width="100%" align="center">
-            <Heading {...styles.heading}>You are {capitalize(blockchain)} holder</Heading>
-            <Heading as="h3" {...styles.heading} {...styles.counter}>
-              <Text as="span" marginRight="0.6rem">
-                #
-              </Text>
-              <Countup
-                start={totalAccounts}
-                end={rank}
-                duration={2.75}
-                separator={getIntegerSeparator(getUserLocale())}
-              />
-            </Heading>
+            {blockchain === "cardano" && (
+              <>
+                <Heading {...styles.heading}>Cardano Rankings</Heading>
+                <Heading as="h3" {...styles.heading} {...styles.counter}>
+                  are coming soon
+                </Heading>
+              </>
+            )}
+
+            {blockchain !== "cardano" && (
+              <>
+                <Heading {...styles.heading}>You are {capitalize(blockchain)} holder</Heading>
+                <Heading as="h3" {...styles.heading} {...styles.counter}>
+                  <Text as="span" marginRight="0.6rem">
+                    #
+                  </Text>
+                  <Countup
+                    start={totalAccounts}
+                    end={rank}
+                    duration={2.75}
+                    separator={getIntegerSeparator(getUserLocale())}
+                  />
+                </Heading>
+              </>
+            )}
           </Stack>
         </Center>
       </Container>
