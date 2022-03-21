002557a4b8013ab25d5e9bee4960908bf2408014
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-03-21 22:29:01 +0100

    Start using Poppins font for headings

--- app/core/theme/foundations/fonts.ts
+++ app/core/theme/foundations/fonts.ts
@@ -1,6 +1,8 @@
 // import external fonts
 import "@fontsource/inter"
+import "@fontsource/poppins"
 
 export const fonts = {
   body: "Inter, sans-serif",
+  heading: "Poppins", // font used by chakra headings
 }
