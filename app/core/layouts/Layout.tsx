27816a68457aa9cda4ecf3f247694a51b0e7f10c
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-03-22 14:46:26 +0100

    Adds Google Analytics

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex } from "@chakra-ui/react"
 import { Footer, Navbar } from "@components"
-import { BlitzLayout, Head } from "blitz"
+import { BlitzLayout, Head, Script } from "blitz"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
@@ -9,6 +9,19 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
         <title>{title || "wallet-rankings"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-X0651PNQQN" />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X0651PNQQN');
+        `}
+      </Script>
 
       {/* This flex keeps the footer nicely at bottom. See // https://stackoverflow.com/a/66565163/9850103 */}
       <Flex display="flex" minHeight="100vh" direction="column" justifyContent="space-between">
