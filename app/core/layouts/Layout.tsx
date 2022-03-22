8636106b1d61674f9bca838573f6913525d59829
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-03-22 15:57:38 +0100

    Start using async GA tagmanager

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
       </Head>
 
       {/* Global site tag (gtag.js) - Google Analytics */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-X0651PNQQN" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X0651PNQQN" />
 
       <Script id="google-analytics" strategy="afterInteractive">
         {`
