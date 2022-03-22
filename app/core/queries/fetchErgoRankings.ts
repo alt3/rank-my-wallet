933f8c978864da5a5acbe80119286de3c74f3ebd
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-03-22 18:02:51 +0100

    Fix ranking-bug for ergo top-dog and bottom-dog

--- app/core/queries/fetchErgoRankings.ts
+++ app/core/queries/fetchErgoRankings.ts
@@ -7,24 +7,30 @@ export default async function fetchErgoRankings(parsedAddress) {
   return await axios.get(url, config).then((response) => {
     const rankings = [
       {
-        position: "next",
-        rank: response.data.above.rank,
-        balance: response.data.above.balance,
-        address: response.data.above.address,
-      },
-      {
         position: "current",
         rank: response.data.target.rank,
         balance: response.data.target.balance,
         address: response.data.target.address,
       },
-      {
+    ]
+
+    if (response.data.above !== null) {
+      rankings.push({
+        position: "next",
+        rank: response.data.above.rank,
+        balance: response.data.above.balance,
+        address: response.data.above.address,
+      })
+    }
+
+    if (response.data.under !== null) {
+      rankings.push({
         position: "previous",
         rank: response.data.under.rank,
         balance: response.data.under.balance,
         address: response.data.under.address,
-      },
-    ]
+      })
+    }
 
     return rankings
   })
