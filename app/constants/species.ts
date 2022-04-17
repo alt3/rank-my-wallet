508c61160a64cc604db71ecb1c2fd024bae562e8
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-04-17 20:19:31 +0200

    Adds Image folder for species

--- app/constants/species.ts
+++ app/constants/species.ts
@@ -1,88 +1,110 @@
 const species = {
+  // https://datastudio.google.com/reporting/3136c55b-635e-4f46-8e4b-b8ab54f2d460/page/r2LQC
   cardano: [
     {
       startsAt: 0,
       name: "ghost",
+      icon: "Ghost",
     },
     {
       startsAt: 0.000001,
       name: "plankton",
+      icon: "Plankton",
     },
     {
       startsAt: 1,
       name: "seahorse",
+      icon: "Seahorse",
     },
     {
       startsAt: 10,
       name: "pipefish",
+      icon: "Pipefish",
     },
     {
       startsAt: 25,
       name: "shrimp",
+      icon: "Shrimp",
     },
     {
       startsAt: 100,
       name: "shell",
+      icon: "Shell",
     },
     {
       startsAt: 500,
       name: "oyster",
+      icon: "Oyster",
     },
     {
       startsAt: 1000,
       name: "starfish",
+      icon: "Starfish",
     },
     {
       startsAt: 2000,
       name: "crab",
+      icon: "Crab",
     },
     {
       startsAt: 5000,
       name: "fish",
+      icon: "Fish",
     },
     {
       startsAt: 10000,
       name: "jellyfish",
+      icon: "Jellyfish",
     },
     {
       startsAt: 50000,
       name: "piranha",
+      icon: "Piranha",
     },
     {
       startsAt: 100000,
       name: "swordfish",
+      icon: "Swordfish",
     },
     {
       startsAt: 150000,
       name: "octopus",
+      icon: "Octopus",
     },
     {
       startsAt: 400000,
       name: "shark",
+      icon: "Shark",
     },
     {
       startsAt: 750000,
       name: "tiger shark",
+      icon: "Shark",
     },
     {
       startsAt: 1000000,
       name: "great white shark",
+      icon: "Shark",
     },
     {
       startsAt: 5000000,
       name: "orca",
+      icon: "Orca",
     },
     {
       startsAt: 10000000,
       name: "whale",
+      icon: "Whale",
     },
     {
       startsAt: 50000000,
       name: "fin whale",
+      icon: "Whale",
     },
     {
       startsAt: 100000000,
       name: "blue whale",
+      icon: "Whale",
     },
   ],
 
@@ -90,86 +112,107 @@ const species = {
     {
       startsAt: 0,
       name: "ghost",
+      icon: "Ghost",
     },
     {
       startsAt: 0.000000001,
       name: "plankton",
+      icon: "Plankton",
     },
     {
       startsAt: 1,
       name: "seahorse",
+      icon: "Seahorse",
     },
     {
       startsAt: 10,
       name: "pipefish",
+      icon: "Pipefish",
     },
     {
       startsAt: 25,
       name: "shrimp",
+      icon: "Shrimp",
     },
     {
       startsAt: 50,
       name: "shell",
+      icon: "Shell",
     },
     {
       startsAt: 100,
       name: "oyster",
+      icon: "Oyster",
     },
     {
       startsAt: 250,
       name: "starfish",
+      icon: "Starfish",
     },
     {
       startsAt: 500,
       name: "crab",
+      icon: "Crab",
     },
     {
       startsAt: 1000,
       name: "fish",
+      icon: "Fish",
     },
     {
       startsAt: 2500,
       name: "jellyfish",
+      icon: "Jellyfish",
     },
     {
       startsAt: 5000,
       name: "piranha",
+      icon: "Piranha",
     },
     {
       startsAt: 10000,
       name: "swordfish",
+      icon: "Starfish",
     },
     {
       startsAt: 25000,
       name: "octopus",
+      icon: "Octopus",
     },
     {
       startsAt: 50000,
       name: "shark",
+      icon: "Shark",
     },
     {
       startsAt: 100000,
       name: "tiger shark",
+      icon: "Shark",
     },
     {
       startsAt: 250000,
       name: "great white shark",
+      icon: "Shark",
     },
     {
       startsAt: 500000,
       name: "orca",
+      icon: "Orca",
     },
     {
       startsAt: 1000000,
       name: "whale",
+      icon: "Whale",
     },
     {
       startsAt: 2500000,
       name: "fin whale",
+      icon: "Whale",
     },
     {
       startsAt: 5000000,
       name: "blue whale",
+      icon: "Whale",
     },
   ],
 }
