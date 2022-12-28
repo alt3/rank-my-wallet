48a4407069dd568f54e153e1310b946aed7a7bb6
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-12-28 16:35:31 +0100

    Bumps blitz to 2.0.0-beta.20 with decoupled auth (#31)

--- app/blitz-client.ts
+++ app/blitz-client.ts
@@ -1,12 +1,6 @@
-import { AuthClientPlugin } from "@blitzjs/auth"
 import { setupBlitzClient } from "@blitzjs/next"
 import { BlitzRpcPlugin } from "@blitzjs/rpc"
 
-// @todo remove when RPC and Auth are decoupled https://github.com/blitz-js/blitz/pull/3943
-export const authConfig = {
-  cookiePrefix: "blitz-next2",
-}
-
 export const { withBlitz } = setupBlitzClient({
-  plugins: [AuthClientPlugin(authConfig), BlitzRpcPlugin({})],
+  plugins: [BlitzRpcPlugin({})],
 })
