a4d1252528e320b298701368da8bab2d4b1a0677
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-12-28 16:22:13 +0100

    Bumps blitz to 2.0.0-beta.20 with decoupled auth

--- app/blitz-server.ts
+++ app/blitz-server.ts
@@ -1,16 +1,7 @@
 import { setupBlitzServer } from "@blitzjs/next"
-import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
-import { simpleRolesIsAuthorized } from "@blitzjs/auth"
 import { BlitzLogger } from "blitz"
-import { authConfig } from "./blitz-client"
 
 export const { gSSP, gSP, api } = setupBlitzServer({
-  plugins: [
-    // AuthServerPlugin({
-    //   ...authConfig,
-    //   storage: PrismaStorage(db),
-    //   isAuthorized: simpleRolesIsAuthorized,
-    // }),
-  ],
+  plugins: [],
   logger: BlitzLogger({}),
 })
