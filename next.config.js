b0aa42df5d1133365980ccf27bc9da47442d8a45
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-01-07 11:14:32 +0100

    Run Nextjs bundle analyzer conditionally

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 // @ts-check
 const { withBlitz } = require("@blitzjs/next")
 
+// analyze
+const shouldAnalyzeBundles = process.env.ANALYZE === "true"
+const withBundleAnalyzer = shouldAnalyzeBundles
+  ? require("@next/bundle-analyzer")({ enabled: true })
+  : (config) => config
+if (shouldAnalyzeBundles) console.log("Analyze!!!")
+
 const config = {
   // experimental: { appDir: true },
 }
 
-module.exports = withBlitz(config)
+module.exports = withBlitz(withBundleAnalyzer(config))
