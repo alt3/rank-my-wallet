f13de92f64550d36519ba7c1360251ee8ebcb9a8
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-01-20 09:15:36 +0100

    Bump bs58 to v5

--- src/core/lib/utils/bytes/getFirstByte.ts
+++ src/core/lib/utils/bytes/getFirstByte.ts
@@ -1,8 +1,8 @@
 /**
  * Returns the first byte from an array of bytes.
  *
- * @param bytes - Buffer with bytes array
+ * @param bytes - Uint8Array with bytes array
  */
-export const getFirstByte = function (bytes: Buffer): Buffer {
+export const getFirstByte = function (bytes: Uint8Array): Uint8Array {
   return bytes.slice(0, 1)
 }
