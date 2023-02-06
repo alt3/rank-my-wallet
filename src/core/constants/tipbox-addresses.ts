13ce67ae2472d2200bc4e55db710ecb422a54fc2
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-02-06 20:36:09 +0100

    Remove known-donators condition from PleaseDonate component

--- src/core/constants/tipbox-addresses.ts
+++ src/core/constants/tipbox-addresses.ts
@@ -2,11 +2,6 @@ const tipboxAddresses = {
   cardano:
     "addr1q9652azuc2cme8u3hkuleqjcqrc25v58szupwke3mceayw4xastpuepx8ue6pcf4tehnmp84qywvllauwgwpw60u6gcs730n0c",
   ergo: "9h1cuw5d6TJb6cGgrqfPHkFJcjPPaARTy5dmucanqtranQ2xRMu",
-  donators: [
-    "9hvLmZHtSTgBEGXcjGiHNHujYJtsjQufbK9znTASKhjVVvDJDe2",
-    "9fvCAYUCdCXeA1d2ssKiyxzsaAPSMiYrTirPCfdZFxLU9yZZFpj",
-    "9hVmoLU4H3Ru6QNGmiZjSGDwZj8PN7RBxjBjcjNJLbUQWhCpUQC",
-  ],
 }
 
 export default tipboxAddresses
