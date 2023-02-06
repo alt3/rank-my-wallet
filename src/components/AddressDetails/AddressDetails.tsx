6dafcdc61bc5c5f587a00a98c2593dfb9eb17c27
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-02-06 17:13:03 +0100

    Show blockchain name on address page title (#70) (closes #4)
    * Show blockchain name on address page title (closes #4)
    
    * Fix page title while loading address details

--- src/components/AddressDetails/AddressDetails.tsx
+++ src/components/AddressDetails/AddressDetails.tsx
@@ -1,6 +1,8 @@
-import getAddressDetails from "src/core/queries/getAddressDetails"
-import { useQuery } from "@blitzjs/rpc"
 import { useParam } from "@blitzjs/next"
+import { useQuery } from "@blitzjs/rpc"
+import Head from "next/head"
+import { capitalize } from "src/core/lib/utils"
+import getAddressDetails from "src/core/queries/getAddressDetails"
 import SupportedAddressDetails from "./SupportedAddressDetails"
 import UnsupportedAddressDetails from "./UnsupportedAddressDetails"
 
@@ -10,10 +12,35 @@ export const AddressDetails = () => {
     staleTime: Infinity,
   })
 
+  const pageTitle = `Rank My Wallet - Your ${capitalize(
+    addressDetails.parsed.blockchain.name
+  )} Rank`
+
+  const metaData = {
+    title: `Rank My Wallet - Your ${capitalize(addressDetails.parsed.blockchain.name)} Rank`,
+    description: `Your ${capitalize(
+      addressDetails.parsed.blockchain.name
+    )} blockchain ranking and address analysis`,
+    keywords: `${addressDetails.parsed.blockchain.name}, blockchain, wallets, rankings, address-analyzer, species`,
+    url: `https://rankmywallet.com/${address}`,
+  }
+
   // console.log(addressDetails)
 
   return (
     <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={metaData.description} />
+        <meta name="keywords" content={metaData.keywords} />
+        <meta property="og:title" content={pageTitle} key="title" />
+        <meta property="og:url" content={metaData.url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={metaData.description} />
+        <meta name="twitter:content" content="summary" />
+        <meta name="twitter:site" content="@RankMyWallet" />
+      </Head>
+
       {addressDetails.parsed.isSupported === false && (
         <UnsupportedAddressDetails parsed={addressDetails.parsed} />
       )}
--- src/pages/[address].tsx
+++ src/pages/[address].tsx
@@ -1,32 +1,17 @@
-import { AddressDetails, SuspenseLoader } from "src/components"
-import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
 import Head from "next/head"
 import { Suspense } from "react"
+import { AddressDetails, SuspenseLoader } from "src/components"
+import Layout from "src/core/layouts/Layout"
 
 const ShowRankingPage: BlitzPage = () => {
-  const meta = {
-    title: "Rank My Wallet - Rankings",
-    description: "Your blockchain ranking and address analysis",
-    keywords: "rankings, cardano, ergo, blockchain, addresses, wallets, species, analyser, online",
-    url: "https://rankmywallet.com/rankings",
-  }
+  const pageTitle = "Rank My Wallet"
 
   return (
     <>
       <Head>
-        <title>{meta.title}</title>
-        <meta name="description" content={meta.description} />
-        <meta name="keywords" content={meta.keywords} />
-        <meta property="og:title" content={meta.title} key="title" />
-        <meta property="og:url" content={meta.url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:description" content={meta.description} />
-        <meta name="twitter:content" content="summary" />
-        <meta name="twitter:site" content="@RankMyWallet" />
+        <title>{pageTitle}</title>
       </Head>
-
       <div>
         <Suspense fallback={<SuspenseLoader />}>
           <AddressDetails />
