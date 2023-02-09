1c09fac14647031fcaf3a4f378981315a5983ddc
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-02-09 19:36:25 +0100

    Fix unexpected error (closes #87) (#90)

--- src/components/AddressDetails/UnsupportedAddressDetails.tsx
+++ src/components/AddressDetails/UnsupportedAddressDetails.tsx
@@ -18,6 +18,7 @@ import {
   DataGrid,
   DataGridEntry,
   ErrorHero,
+  MetaTags,
   SectionHeader,
 } from "src/components"
 import { capitalize } from "src/lib"
@@ -27,112 +28,123 @@ export function UnsupportedAddressDetails({ parsed }) {
   const colorTeal = useColorModeValue("teal.500", "teal.300")
 
   return (
-    <Container maxW="container.md" marginBottom="2.5rem">
-      <ErrorHero title="Unsupported Address" subtitle={parsed.unsupported.message}></ErrorHero>
-
-      <AddressForm
-        placeholder="Try again?"
-        marginTop={{ base: "3rem", sm: "5rem" }}
-        marginBottom={{ base: "4rem", sm: "7rem" }}
+    <>
+      <MetaTags
+        title={`Rank My Wallet - Unrecognized Address`}
+        description={`Unrecognized Address`}
+        keywords={`blockchain, wallets, rankings, species, address-analyzer`}
       />
 
-      {["cardano", "ergo"].includes(parsed.blockchain.name) && (
-        <>
-          <Divider marginBottom={{ base: "1.5rem", sm: "1.5rem" }} />
+      <Container maxW="container.md" marginBottom="2.5rem">
+        <ErrorHero title="Unsupported Address" subtitle={parsed.unsupported.message}></ErrorHero>
 
-          <Accordion allowMultiple>
-            <AccordionItem borderStyle="none" marginBottom={{ base: "1rem", sm: "0.5rem" }}>
-              <h2>
+        <AddressForm
+          placeholder="Try again?"
+          marginTop={{ base: "3rem", sm: "5rem" }}
+          marginBottom={{ base: "4rem", sm: "7rem" }}
+        />
+
+        {["cardano", "ergo"].includes(parsed.blockchain.name) && (
+          <>
+            <Divider marginBottom={{ base: "1.5rem", sm: "1.5rem" }} />
+
+            <Accordion allowMultiple>
+              <AccordionItem borderStyle="none" marginBottom={{ base: "1rem", sm: "0.5rem" }}>
+                <h2>
+                  <AccordionButton p={0}>
+                    <Box flex="1" textAlign="left">
+                      <SectionHeader>Address Details</SectionHeader>
+                    </Box>
+                    <Box as="span" verticalAlign="top" minHeight="3rem">
+                      <AccordionIcon color={colorTeal} />
+                    </Box>
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel p={0} pb="2rem">
+                  <DataGrid marginBottom={{ base: "1.5rem", sm: "1.5rem" }}>
+                    <DataGridEntry field="Address" value={parsed.address} />
+                    <DataGridEntry
+                      field="Blockchain"
+                      value={`${capitalize(parsed.blockchain.name)} (${parsed.currency.ticker})`}
+                    />
+                    {parsed.version && (
+                      <DataGridEntry field="Address Version" value={capitalize(parsed.version)} />
+                    )}
+                    {parsed.blockchain.network && (
+                      <DataGridEntry
+                        field="Network"
+                        value={capitalize(parsed.blockchain.network)}
+                      />
+                    )}
+                    {parsed.type && (
+                      <DataGridEntry field="Address Type" value={capitalize(parsed.type.name)} />
+                    )}
+                  </DataGrid>
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem borderStyle="none">
                 <AccordionButton p={0}>
                   <Box flex="1" textAlign="left">
-                    <SectionHeader>Address Details</SectionHeader>
+                    <SectionHeader>Address Analysis</SectionHeader>
                   </Box>
                   <Box as="span" verticalAlign="top" minHeight="3rem">
                     <AccordionIcon color={colorTeal} />
                   </Box>
                 </AccordionButton>
-              </h2>
-              <AccordionPanel p={0} pb="2rem">
-                <DataGrid marginBottom={{ base: "1.5rem", sm: "1.5rem" }}>
-                  <DataGridEntry field="Address" value={parsed.address} />
-                  <DataGridEntry
-                    field="Blockchain"
-                    value={`${capitalize(parsed.blockchain.name)} (${parsed.currency.ticker})`}
-                  />
-                  {parsed.version && (
-                    <DataGridEntry field="Address Version" value={capitalize(parsed.version)} />
+
+                <AccordionPanel p={0} pb={4}>
+                  <DataGrid>
+                    <DataGridEntry field="Encoding" value={capitalize(parsed.encoding)} />
+                    {parsed.decoded !== undefined && parsed.decoded.prefix !== undefined && (
+                      <DataGridEntry field="Decoded Prefix" value={parsed.decoded.prefix} />
+                    )}
+
+                    {parsed.header !== undefined && (
+                      <DataGridEntry field="Header Byte" value={parsed.header.byte} />
+                    )}
+                  </DataGrid>
+
+                  <Text color={colorTeal} marginBottom="0.25rem">
+                    Decoded Bytes:
+                  </Text>
+                  <Grid
+                    templateColumns={{ base: "repeat(4, 1fr)", sm: "repeat(6, 1fr)" }}
+                    paddingBottom="1.5rem"
+                  >
+                    {parsed.decoded.bytes.map((byte) => (
+                      <GridItem key={nextId("byte")}>{byte}</GridItem>
+                    ))}
+                  </Grid>
+
+                  {parsed.payload.prefix !== undefined && (
+                    <>
+                      <BitsTable
+                        caption="Prefix Bits:"
+                        bits={parsed.payload.prefix.bits}
+                        sumLabel="Header Byte"
+                      ></BitsTable>
+
+                      <BitsTable
+                        caption="Leading Prefix Bits:"
+                        bits={parsed.payload.prefix.leading.bits}
+                        sumLabel={parsed.payload.prefix.leading.type}
+                      ></BitsTable>
+
+                      <BitsTable
+                        caption="Trailing Prefix Bits:"
+                        bits={parsed.payload.prefix.trailing.bits}
+                        sumLabel={parsed.payload.prefix.trailing.type}
+                      ></BitsTable>
+                    </>
                   )}
-                  {parsed.blockchain.network && (
-                    <DataGridEntry field="Network" value={capitalize(parsed.blockchain.network)} />
-                  )}
-                  {parsed.type && (
-                    <DataGridEntry field="Address Type" value={capitalize(parsed.type.name)} />
-                  )}
-                </DataGrid>
-              </AccordionPanel>
-            </AccordionItem>
-
-            <AccordionItem borderStyle="none">
-              <AccordionButton p={0}>
-                <Box flex="1" textAlign="left">
-                  <SectionHeader>Address Analysis</SectionHeader>
-                </Box>
-                <Box as="span" verticalAlign="top" minHeight="3rem">
-                  <AccordionIcon color={colorTeal} />
-                </Box>
-              </AccordionButton>
-
-              <AccordionPanel p={0} pb={4}>
-                <DataGrid>
-                  <DataGridEntry field="Encoding" value={capitalize(parsed.encoding)} />
-                  {parsed.decoded !== undefined && parsed.decoded.prefix !== undefined && (
-                    <DataGridEntry field="Decoded Prefix" value={parsed.decoded.prefix} />
-                  )}
-
-                  {parsed.header !== undefined && (
-                    <DataGridEntry field="Header Byte" value={parsed.header.byte} />
-                  )}
-                </DataGrid>
-
-                <Text color={colorTeal} marginBottom="0.25rem">
-                  Decoded Bytes:
-                </Text>
-                <Grid
-                  templateColumns={{ base: "repeat(4, 1fr)", sm: "repeat(6, 1fr)" }}
-                  paddingBottom="1.5rem"
-                >
-                  {parsed.decoded.bytes.map((byte) => (
-                    <GridItem key={nextId("byte")}>{byte}</GridItem>
-                  ))}
-                </Grid>
-
-                {parsed.payload.prefix !== undefined && (
-                  <>
-                    <BitsTable
-                      caption="Prefix Bits:"
-                      bits={parsed.payload.prefix.bits}
-                      sumLabel="Header Byte"
-                    ></BitsTable>
-
-                    <BitsTable
-                      caption="Leading Prefix Bits:"
-                      bits={parsed.payload.prefix.leading.bits}
-                      sumLabel={parsed.payload.prefix.leading.type}
-                    ></BitsTable>
-
-                    <BitsTable
-                      caption="Trailing Prefix Bits:"
-                      bits={parsed.payload.prefix.trailing.bits}
-                      sumLabel={parsed.payload.prefix.trailing.type}
-                    ></BitsTable>
-                  </>
-                )}
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
-        </>
-      )}
-    </Container>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </>
+        )}
+      </Container>
+    </>
   )
 }
 
