de4bd0c7f9b4cd23e44070409350bcf9b1a184bf
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-02-09 18:26:18 +0100

    Fix unexpected error (closes #87)

--- src/components/AddressDetails/AddressDetails.tsx
+++ src/components/AddressDetails/AddressDetails.tsx
@@ -1,7 +1,5 @@
 import { useParam } from "@blitzjs/next"
 import { useQuery } from "@blitzjs/rpc"
-import { MetaTags } from "src/components"
-import { capitalize } from "src/lib"
 import getAddressDetails from "src/core/queries/getAddressDetails"
 import SupportedAddressDetails from "./SupportedAddressDetails"
 import UnsupportedAddressDetails from "./UnsupportedAddressDetails"
@@ -16,14 +14,6 @@ export const AddressDetails = () => {
 
   return (
     <>
-      <MetaTags
-        title={`Rank My Wallet - Your ${capitalize(addressDetails.parsed.blockchain.name)} Rank`}
-        description={`Your ${capitalize(
-          addressDetails.parsed.blockchain.name
-        )} blockchain ranking and address analysis`}
-        keywords={`blockchain, ${addressDetails.parsed.blockchain.name}, wallets, rankings, species, address-analyzer`}
-      />
-
       {addressDetails.parsed.isSupported === false && (
         <UnsupportedAddressDetails parsed={addressDetails.parsed} />
       )}
--- src/components/AddressDetails/SupportedAddressDetails.tsx
+++ src/components/AddressDetails/SupportedAddressDetails.tsx
@@ -20,6 +20,7 @@ import {
   DataGridEntry,
   ExternalLinkIcon,
   Link,
+  MetaTags,
   PleaseDonate,
   RankingsTable,
   SectionHeader,
@@ -83,88 +84,127 @@ export function SupportedAddressDetails({ parsed, addressCount, balance, species
   }
 
   return (
-    <Container maxW="container.md" marginBottom="2.5rem">
-      <Counter
-        blockchain={parsed.blockchain.name}
-        totalAccounts={addressCount}
-        rank={rankings.find(({ position }) => position === "current").rank}
-        balance={balance.nano}
-      ></Counter>
+    <>
+      <MetaTags
+        title={`Rank My Wallet - Your ${capitalize(parsed.blockchain.name)} Rank`}
+        description={`Your ${capitalize(
+          parsed.blockchain.name
+        )} blockchain ranking and address analysis`}
+        keywords={`blockchain, ${parsed.blockchain.name}, wallets, rankings, species, address-analyzer`}
+      />
 
-      <Divider display={{ base: "block", sm: "none" }} marginBottom="2rem" />
+      <Container maxW="container.md" marginBottom="2.5rem">
+        <Counter
+          blockchain={parsed.blockchain.name}
+          totalAccounts={addressCount}
+          rank={rankings.find(({ position }) => position === "current").rank}
+          balance={balance.nano}
+        ></Counter>
 
-      {/* SPECIES GRID */}
-      <SectionHeader>Species</SectionHeader>
-      <Box minHeight="100px" height="auto">
-        <Grid gap={0} marginBottom={{ base: ".5rem", sm: ".75rem" }}>
-          <GridItem {...styles.gridField}>Your Balance</GridItem>
-          <GridItem {...styles.gridValue}>
-            {" "}
-            <TickerString
-              ticker={bigToString(balance.ticker, 0)}
-              tickerSymbol={parsed.currency.tickerSymbol}
-            ></TickerString>
-          </GridItem>
+        <Divider display={{ base: "block", sm: "none" }} marginBottom="2rem" />
 
-          <GridItem {...styles.gridField}>Current Species</GridItem>
-          <GridItem {...styles.gridValue}>
-            <Link
-              href={`/species/${parsed.blockchain.name}`}
-              title={`${capitalize(parsed.blockchain.name)} Species`}
-              color={useColorModeValue("pink.600", "pink.400")}
-              passHref
+        {/* SPECIES GRID */}
+        <SectionHeader>Species</SectionHeader>
+        <Box minHeight="100px" height="auto">
+          <Grid gap={0} marginBottom={{ base: ".5rem", sm: ".75rem" }}>
+            <GridItem {...styles.gridField}>Your Balance</GridItem>
+            <GridItem {...styles.gridValue}>
+              {" "}
+              <TickerString
+                ticker={bigToString(balance.ticker, 0)}
+                tickerSymbol={parsed.currency.tickerSymbol}
+              ></TickerString>
+            </GridItem>
+
+            <GridItem {...styles.gridField}>Current Species</GridItem>
+            <GridItem {...styles.gridValue}>
+              <Link
+                href={`/species/${parsed.blockchain.name}`}
+                title={`${capitalize(parsed.blockchain.name)} Species`}
+                color={useColorModeValue("pink.600", "pink.400")}
+                passHref
+              >
+                {capitalize(species.current.name)} <ExternalLinkIcon />
+              </Link>
+            </GridItem>
+
+            <GridItem {...styles.gridField}>Next Species</GridItem>
+            <GridItem {...styles.gridValue}>{capitalize(species.next.name)}</GridItem>
+
+            <GridItem {...styles.gridField}>Starts At</GridItem>
+            <GridItem {...styles.gridValue}>
+              {" "}
+              <TickerString
+                ticker={bigToString(species.next.startsAt, 0)}
+                tickerSymbol={parsed.currency.tickerSymbol}
+              ></TickerString>
+            </GridItem>
+
+            <GridItem {...styles.gridField}>Requires</GridItem>
+            <GridItem {...styles.gridValue}>
+              {" "}
+              <TickerString
+                ticker={bigToString(species.next.requires, parsed.currency.decimals)}
+                tickerSymbol={parsed.currency.tickerSymbol}
+                fractionsColor={fractionsColor}
+              ></TickerString>
+            </GridItem>
+
+            {/* Species Image */}
+            <GridItem
+              rowStart={{ base: 1, sm: 1 }}
+              rowSpan={{ base: 6, sm: 6 }}
+              colStart={{ base: 9, sm: 9 }}
+              colSpan={{ base: 4, sm: 4 }}
             >
-              {capitalize(species.current.name)} <ExternalLinkIcon />
-            </Link>
-          </GridItem>
+              <SpeciesImageComponent
+                height={{ base: "100px", sm: "75%" }}
+                marginLeft={{ base: "inherit", sm: "auto" }}
+              />
+            </GridItem>
+          </Grid>
+        </Box>
 
-          <GridItem {...styles.gridField}>Next Species</GridItem>
-          <GridItem {...styles.gridValue}>{capitalize(species.next.name)}</GridItem>
+        <PleaseDonate marginBottom={{ base: "2rem", sm: "2rem" }} />
 
-          <GridItem {...styles.gridField}>Starts At</GridItem>
-          <GridItem {...styles.gridValue}>
-            {" "}
-            <TickerString
-              ticker={bigToString(species.next.startsAt, 0)}
-              tickerSymbol={parsed.currency.tickerSymbol}
-            ></TickerString>
-          </GridItem>
+        <Accordion allowMultiple>
+          {/* COMPETITION PANE - IF APPLICABLE */}
+          {rankings.length > 1 && (
+            <AccordionItem borderStyle="none" marginBottom={{ base: "1rem", sm: "0.5rem" }}>
+              <h2>
+                <AccordionButton p={0}>
+                  <Box flex="1" textAlign="left">
+                    <SectionHeader>Competition</SectionHeader>
+                  </Box>
+                  <Box as="span" verticalAlign="top" minHeight="3rem">
+                    <AccordionIcon color={accordionIconColor} />
+                  </Box>
+                </AccordionButton>
+              </h2>
+              <AccordionPanel p={0} pb="2rem">
+                <DataGrid marginBottom={{ base: "0.5rem", sm: "0.5rem" }}>
+                  <DataGridEntry
+                    field="Competitors"
+                    value={bigToString(addressCount, 0)}
+                    url={{
+                      href: "https://ergo.watch/metrics/addresses",
+                      title: "Ergo Watch address metrics",
+                      isExternal: true,
+                    }}
+                    linkIcon={true}
+                  />
+                </DataGrid>
+                <RankingsTable rankings={rankings} />
+              </AccordionPanel>
+            </AccordionItem>
+          )}
 
-          <GridItem {...styles.gridField}>Requires</GridItem>
-          <GridItem {...styles.gridValue}>
-            {" "}
-            <TickerString
-              ticker={bigToString(species.next.requires, parsed.currency.decimals)}
-              tickerSymbol={parsed.currency.tickerSymbol}
-              fractionsColor={fractionsColor}
-            ></TickerString>
-          </GridItem>
-
-          {/* Species Image */}
-          <GridItem
-            rowStart={{ base: 1, sm: 1 }}
-            rowSpan={{ base: 6, sm: 6 }}
-            colStart={{ base: 9, sm: 9 }}
-            colSpan={{ base: 4, sm: 4 }}
-          >
-            <SpeciesImageComponent
-              height={{ base: "100px", sm: "75%" }}
-              marginLeft={{ base: "inherit", sm: "auto" }}
-            />
-          </GridItem>
-        </Grid>
-      </Box>
-
-      <PleaseDonate marginBottom={{ base: "2rem", sm: "2rem" }} />
-
-      <Accordion allowMultiple>
-        {/* COMPETITION PANE - IF APPLICABLE */}
-        {rankings.length > 1 && (
+          {/* ADDRESS DETAILS PANE */}
           <AccordionItem borderStyle="none" marginBottom={{ base: "1rem", sm: "0.5rem" }}>
             <h2>
               <AccordionButton p={0}>
                 <Box flex="1" textAlign="left">
-                  <SectionHeader>Competition</SectionHeader>
+                  <SectionHeader>Address Details</SectionHeader>
                 </Box>
                 <Box as="span" verticalAlign="top" minHeight="3rem">
                   <AccordionIcon color={accordionIconColor} />
@@ -172,135 +212,106 @@ export function SupportedAddressDetails({ parsed, addressCount, balance, species
               </AccordionButton>
             </h2>
             <AccordionPanel p={0} pb="2rem">
-              <DataGrid marginBottom={{ base: "0.5rem", sm: "0.5rem" }}>
+              <DataGrid marginBottom={{ base: "1.5rem", sm: "1.5rem" }}>
                 <DataGridEntry
-                  field="Competitors"
-                  value={bigToString(addressCount, 0)}
+                  field="Address"
+                  value={parsed.address}
                   url={{
-                    href: "https://ergo.watch/metrics/addresses",
-                    title: "Ergo Watch address metrics",
+                    href: `${parsed.blockchain.explorerUrl + parsed.address}`,
+                    title: `${capitalize(parsed.blockchain.name)} Explorer`,
                     isExternal: true,
                   }}
-                  linkIcon={true}
                 />
+
+                <DataGridEntry
+                  field="Blockchain"
+                  value={`${capitalize(parsed.blockchain.name)} (${parsed.currency.ticker})`}
+                />
+
+                {parsed.blockchain.network && (
+                  <DataGridEntry field="Network" value={capitalize(parsed.blockchain.network)} />
+                )}
+
+                {parsed.version && (
+                  <DataGridEntry field="Version" value={capitalize(parsed.version)} />
+                )}
+
+                {parsed.type && (
+                  <DataGridEntry field="Address Type" value={capitalize(parsed.type.name)} />
+                )}
               </DataGrid>
-              <RankingsTable rankings={rankings} />
             </AccordionPanel>
           </AccordionItem>
-        )}
 
-        {/* ADDRESS DETAILS PANE */}
-        <AccordionItem borderStyle="none" marginBottom={{ base: "1rem", sm: "0.5rem" }}>
-          <h2>
+          {/* ADDRESS ANALYSIS PANE */}
+          <AccordionItem borderStyle="none">
             <AccordionButton p={0}>
               <Box flex="1" textAlign="left">
-                <SectionHeader>Address Details</SectionHeader>
+                <SectionHeader>Address Analysis</SectionHeader>
               </Box>
               <Box as="span" verticalAlign="top" minHeight="3rem">
                 <AccordionIcon color={accordionIconColor} />
               </Box>
             </AccordionButton>
-          </h2>
-          <AccordionPanel p={0} pb="2rem">
-            <DataGrid marginBottom={{ base: "1.5rem", sm: "1.5rem" }}>
-              <DataGridEntry
-                field="Address"
-                value={parsed.address}
-                url={{
-                  href: `${parsed.blockchain.explorerUrl + parsed.address}`,
-                  title: `${capitalize(parsed.blockchain.name)} Explorer`,
-                  isExternal: true,
-                }}
-              />
 
-              <DataGridEntry
-                field="Blockchain"
-                value={`${capitalize(parsed.blockchain.name)} (${parsed.currency.ticker})`}
-              />
+            <AccordionPanel p={0} pb={4}>
+              <DataGrid>
+                <DataGridEntry field="Encoding" value={capitalize(parsed.encoding)} />
+                {parsed.decoded !== undefined && parsed.decoded.prefix !== undefined && (
+                  <DataGridEntry field="Decoded Prefix" value={parsed.decoded.prefix} />
+                )}
 
-              {parsed.blockchain.network && (
-                <DataGridEntry field="Network" value={capitalize(parsed.blockchain.network)} />
-              )}
+                {parsed.payload.prefix !== undefined && (
+                  <DataGridEntry field="Prefix Byte" value={parsed.payload.prefix.byte} />
+                )}
 
-              {parsed.version && (
-                <DataGridEntry field="Version" value={capitalize(parsed.version)} />
-              )}
+                {parsed.payload.checksum !== undefined && (
+                  <DataGridEntry field="Checksum" value={parsed.payload.checksum.hex} />
+                )}
 
-              {parsed.type && (
-                <DataGridEntry field="Address Type" value={capitalize(parsed.type.name)} />
-              )}
-            </DataGrid>
-          </AccordionPanel>
-        </AccordionItem>
+                {parsed.payload.content !== undefined && (
+                  <DataGridEntry field="Content" value={parsed.payload.content.hex} />
+                )}
+              </DataGrid>
 
-        {/* ADDRESS ANALYSIS PANE */}
-        <AccordionItem borderStyle="none">
-          <AccordionButton p={0}>
-            <Box flex="1" textAlign="left">
-              <SectionHeader>Address Analysis</SectionHeader>
-            </Box>
-            <Box as="span" verticalAlign="top" minHeight="3rem">
-              <AccordionIcon color={accordionIconColor} />
-            </Box>
-          </AccordionButton>
-
-          <AccordionPanel p={0} pb={4}>
-            <DataGrid>
-              <DataGridEntry field="Encoding" value={capitalize(parsed.encoding)} />
-              {parsed.decoded !== undefined && parsed.decoded.prefix !== undefined && (
-                <DataGridEntry field="Decoded Prefix" value={parsed.decoded.prefix} />
-              )}
+              <Text color={useColorModeValue("teal.500", "teal.300")} marginBottom="0.25rem">
+                Decoded Bytes:
+              </Text>
+              <Grid
+                templateColumns={{ base: "repeat(4, 1fr)", sm: "repeat(6, 1fr)" }}
+                paddingBottom="1.5rem"
+              >
+                {parsed.decoded.bytes.map((byte) => (
+                  <GridItem key={nextId("byte")}>{byte}</GridItem>
+                ))}
+              </Grid>
 
               {parsed.payload.prefix !== undefined && (
-                <DataGridEntry field="Prefix Byte" value={parsed.payload.prefix.byte} />
+                <>
+                  <BitsTable
+                    caption="Prefix Bits:"
+                    bits={parsed.payload.prefix.bits}
+                    sumLabel="Prefix Byte"
+                  ></BitsTable>
+
+                  <BitsTable
+                    caption="Leading Prefix Bits:"
+                    bits={parsed.payload.prefix.leading.bits}
+                    sumLabel={parsed.payload.prefix.leading.type}
+                  ></BitsTable>
+
+                  <BitsTable
+                    caption="Trailing Prefix Bits:"
+                    bits={parsed.payload.prefix.trailing.bits}
+                    sumLabel={parsed.payload.prefix.trailing.type}
+                  ></BitsTable>
+                </>
               )}
-
-              {parsed.payload.checksum !== undefined && (
-                <DataGridEntry field="Checksum" value={parsed.payload.checksum.hex} />
-              )}
-
-              {parsed.payload.content !== undefined && (
-                <DataGridEntry field="Content" value={parsed.payload.content.hex} />
-              )}
-            </DataGrid>
-
-            <Text color={useColorModeValue("teal.500", "teal.300")} marginBottom="0.25rem">
-              Decoded Bytes:
-            </Text>
-            <Grid
-              templateColumns={{ base: "repeat(4, 1fr)", sm: "repeat(6, 1fr)" }}
-              paddingBottom="1.5rem"
-            >
-              {parsed.decoded.bytes.map((byte) => (
-                <GridItem key={nextId("byte")}>{byte}</GridItem>
-              ))}
-            </Grid>
-
-            {parsed.payload.prefix !== undefined && (
-              <>
-                <BitsTable
-                  caption="Prefix Bits:"
-                  bits={parsed.payload.prefix.bits}
-                  sumLabel="Prefix Byte"
-                ></BitsTable>
-
-                <BitsTable
-                  caption="Leading Prefix Bits:"
-                  bits={parsed.payload.prefix.leading.bits}
-                  sumLabel={parsed.payload.prefix.leading.type}
-                ></BitsTable>
-
-                <BitsTable
-                  caption="Trailing Prefix Bits:"
-                  bits={parsed.payload.prefix.trailing.bits}
-                  sumLabel={parsed.payload.prefix.trailing.type}
-                ></BitsTable>
-              </>
-            )}
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    </Container>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Container>
+    </>
   )
 }
 
