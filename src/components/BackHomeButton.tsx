e37b8fe6d433829b6db03c48407593e69e54b983
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-02-09 19:54:24 +0100

    Update 404 page

--- src/components/BackHomeButton.tsx
+++ src/components/BackHomeButton.tsx
@@ -4,10 +4,8 @@ import { Link } from "src/components"
 export function BackHomeButton({ title, ...rest }) {
   return (
     <Center {...rest}>
-      <Link href="/" title={title} passHref>
-        <Button as="a" colorScheme="teal">
-          {title}
-        </Button>
+      <Link href="/" title={title} textDecoration="none !important" passHref>
+        <Button colorScheme="teal">{title}</Button>
       </Link>
     </Center>
   )
