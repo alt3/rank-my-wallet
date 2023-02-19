72a1991ca0ffd9b09ad47fb5073a67f879970907
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-02-19 11:13:06 +0100

    Translate sponsoring page

--- src/pages/sponsoring.tsx
+++ src/pages/sponsoring.tsx
@@ -2,8 +2,12 @@ import { BlitzPage } from "@blitzjs/next"
 import { Text, useColorModeValue } from "@chakra-ui/react"
 import { ContentContainer, ExternalLinkIcon, Link, MetaTags, PageHero } from "src/components"
 import Layout from "src/core/layouts/Layout"
+import { Trans, t } from "@lingui/macro"
+import { useLingui } from "@lingui/react"
 
 const Sponsoring: BlitzPage = () => {
+  useLingui()
+
   const styles = {
     link: {
       color: useColorModeValue("teal.500", "teal.300"),
@@ -19,30 +23,35 @@ const Sponsoring: BlitzPage = () => {
       />
 
       <ContentContainer>
-        <PageHero title="Become a Sponsor" />
+        <PageHero title={t`Become a sponsor`} />
 
         <Text width="70%" margin="auto" textAlign={"center"} marginBottom={10}>
-          If you are an individual looking to sponsor this project, please consider donating via the{" "}
-          <Link href="/tipbox" aria-label="RankMyWallet Tip Box" {...styles.link}>
-            Tip Box
-          </Link>
-          .
+          <Trans>
+            If you are an individual looking to sponsor this project, please consider donating via
+            the{" "}
+            <Link href="/tipbox" aria-label={t`Tip Box`} {...styles.link}>
+              Tip Box
+            </Link>
+            .
+          </Trans>
         </Text>
 
         <Text width="70%" margin="auto" textAlign={"center"}>
-          If you are a project or business and would like to show your logo on the landing page,
-          contact us on{" "}
-          <Link
-            href="https://www.twitter.com/RankMyWallet/"
-            aria-label="RankMyWallet on Twitter"
-            isExternal
-            passHref
-            withExternalIcon
-            {...styles.link}
-          >
-            Twitter
-          </Link>{" "}
-          for details.
+          <Trans>
+            If you are a project or business and would like to show your logo on the landing page,
+            contact us on{" "}
+            <Link
+              href="https://www.twitter.com/RankMyWallet/"
+              aria-label={t`RankMyWallet on Twitter`}
+              isExternal
+              passHref
+              withExternalIcon
+              {...styles.link}
+            >
+              Twitter
+            </Link>{" "}
+            for more details.
+          </Trans>
         </Text>
       </ContentContainer>
     </>
@@ -50,6 +59,6 @@ const Sponsoring: BlitzPage = () => {
 }
 
 Sponsoring.suppressFirstRenderFlicker = true
-Sponsoring.getLayout = (page) => <Layout title="Tip Box">{page}</Layout>
+Sponsoring.getLayout = (page) => <Layout>{page}</Layout>
 
 export default Sponsoring
