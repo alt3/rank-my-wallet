7af10a6407019492df1783763eba2bb03e3b5809
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-08 10:45:34 +0100

    Add eslint-plugin-string-to-lingui

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,25 @@
-module.exports = require("@blitzjs/next/eslint")
+module.exports = {
+  extends: ["./node_modules/@blitzjs/next/eslint"],
+  plugins: ["eslint-plugin-string-to-lingui"],
+  rules: {
+    "string-to-lingui/t-call-in-function": 2,
+    "string-to-lingui/macro-inside-t-and-i18": 2,
+    "string-to-lingui/t-should-be-before-macro": 2,
+    "string-to-lingui/no-single-varibles-to-translate": 2,
+    "string-to-lingui/forbid-i18n-calls": [
+      2,
+      {
+        rules: [
+          {
+            handlerName: "_",
+            message: "Use t`` from '@lingui/macro' instead",
+          },
+          {
+            handlerName: "number",
+            message: "Use formatCurrency or formatNumber instead",
+          },
+        ],
+      },
+    ],
+  },
+}
