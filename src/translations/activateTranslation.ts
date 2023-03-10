808a4c681c17c3b1f0a75bf6abfd2459010667a2
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-10 16:12:22 +0100

    Remove orphaned make-plural/plurals

--- src/translations/activateTranslation.ts
+++ src/translations/activateTranslation.ts
@@ -1,11 +1,4 @@
 import { i18n } from "@lingui/core"
-import { en, nl } from "make-plural/plurals"
-
-i18n.loadLocaleData({
-  en : {plurals: en},
-  nl: {plurals: nl},
-  pseudo: {plurals: en},
-})
 
 export async function activateTranslation(locale: string) {
   const { messages } = await import(`@lingui/loader!/src/translations/locales/${locale}/messages.po`)
