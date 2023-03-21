8f70285d3f2c1caee9c9743676e29e8855c28eb7
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-21 16:27:33 +0100

    Bump ligui to 4.0.0-next.3, simplify useLinguiInit()

--- src/translations/utils.ts
+++ src/translations/utils.ts
@@ -1,6 +1,5 @@
 import { i18n, Messages } from '@lingui/core';
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
 
 export async function loadCatalog(locale: string) {
   const { messages } = await import(`@lingui/loader!/src/translations/locales/${locale}/messages.po`)
@@ -11,21 +10,7 @@ export async function loadCatalog(locale: string) {
 export function useLinguiInit(messages: Messages) {
   const router = useRouter()
   const locale = router.locale || router.defaultLocale!
-  const firstRender = useRef(true)
-
-  // run only once on the first render (for server side)
-  if (messages && firstRender.current) {
-    i18n.load(locale, messages)
-    i18n.activate(locale)
-    firstRender.current = false
-  }
-
-  useEffect(() => {
-    if (messages) {
-      i18n.load(locale, messages)
-      i18n.activate(locale)
-    }
-  }, [locale, messages])
+  i18n.loadAndActivate(locale, messages, false);
 
   return i18n;
 }
