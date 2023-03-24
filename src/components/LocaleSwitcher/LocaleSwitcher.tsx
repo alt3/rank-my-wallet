ef510de903c480aa1dbdd22691bf002cec4dcc1f
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-24 11:15:25 +0100

    Set country flag using lingui locale

--- src/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,9 +1,14 @@
 import ReactCountryFlag from "react-country-flag"
+import { useLingui } from "@lingui/react"
 
 export function LocaleSwitcher() {
+  const { i18n } = useLingui()
+
+  const country = i18n.locale === "en" ? "us" : "nl"
+
   return (
     <>
-      <ReactCountryFlag countryCode="br" svg />
+      <ReactCountryFlag countryCode={country} svg />
     </>
   )
 }
