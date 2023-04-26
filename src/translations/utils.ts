5b096f55760e1f322cb7e2643a47f599d8439339
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-04-26 18:52:21 +0200

    Bump lingui to v4 💃 (#169)
    * Bump lingui to v4
    
    * Bump vitest

--- src/translations/utils.ts
+++ src/translations/utils.ts
@@ -1,5 +1,6 @@
 import { i18n, Messages } from "@lingui/core"
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export async function loadCatalog(locale: string) {
   const { messages } = await import(`@lingui/loader!./locales/${locale}.po`)
@@ -10,7 +11,23 @@ export async function loadCatalog(locale: string) {
 export function useLinguiInit(messages: Messages) {
   const router = useRouter()
   const locale = router.locale || router.defaultLocale!
-  i18n.loadAndActivate(locale, messages, false)
+  const isClient = typeof window !== "undefined"
+
+  if (!isClient && locale !== i18n.locale) {
+    // there is single instance of i18n on the server
+    i18n.loadAndActivate({ locale, messages })
+  }
+  if (isClient && i18n.locale === undefined) {
+    // first client render
+    i18n.loadAndActivate({ locale, messages })
+  }
+
+  useEffect(() => {
+    const localeDidChange = locale !== i18n.locale
+    if (localeDidChange) {
+      i18n.loadAndActivate({ locale, messages })
+    }
+  }, [locale, messages])
 
   return i18n
 }
