a8c81ce4f0e78118213990b327e6b309e3f4c560
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-05-19 13:53:43 +0200

    Robots.txt: disallow access to routes with multiple locales (#179)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,26 +1,35 @@
+/**
+ * @see { @link https://github.com/iamvishnusankar/next-sitemap#configuration-options}
+ */
+
 const linguiConfig = require("./lingui.config")
+const locales = linguiConfig.locales.filter(function (locale) {
+  return locale !== "pseudo"
+})
 
-const alternateRefs = linguiConfig.locales
-  .filter(function (locale) {
-    return locale !== "pseudo"
-  })
-  .map((locale) => ({
-    href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}`,
-    hreflang: locale,
-  }))
+const alternateRefs = locales.map((locale) => ({
+  href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}`,
+  hreflang: locale,
+}))
 
-const excludedPaths = linguiConfig.locales
-  .filter(function (locale) {
-    return locale !== "pseudo"
-  })
-  .map((locale) => `/${locale}/404`)
-  .concat("/404")
+const exclude = locales.map((locale) => `/${locale}/404`).concat("/404")
+
+const pathsWithMultipleLocales = locales.flatMap((v, i) => locales.map((w) => `/${v}/${w}/`))
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
-  generateRobotsTxt: true,
   generateIndexSitemap: false,
-  alternateRefs: alternateRefs,
-  exclude: excludedPaths,
+  alternateRefs,
+  exclude,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: pathsWithMultipleLocales, // e.g. en-us/nl-nl
+      },
+    ],
+  },
 }
