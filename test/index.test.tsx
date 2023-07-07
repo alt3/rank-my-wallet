30d80db2a8a5a5d68ae91abe0d6a1506cb318370
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-07-07 14:15:55 +0200

    Monthly package bumps (#188)
    * Bump blitz to 30
    
    * Bump next and lingui
    
    * Bump vitest, fix tests
    
    * Fix ts

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,18 +1,19 @@
 /**
  * @vitest-environment jsdom
  */
+import { screen } from "@testing-library/react"
 import { I18nTestingProvider, mockRouter, render } from "test/utils"
 import { test } from "vitest"
 
 import Home from "../src/pages/index"
 
 test("renders expected h2 captions", () => {
-  const { getByText } = render(<Home />, {
+  render(<Home />, {
     router: mockRouter,
     wrapper: I18nTestingProvider,
   })
 
-  expect(getByText(/Supported blockchains:/i)).toBeInTheDocument()
+  expect(screen.getByText(/Supported blockchains:/i)).toBeInTheDocument()
 
-  expect(getByText(/Sponsored by:/i)).toBeInTheDocument()
+  expect(screen.getByText(/Sponsored by:/i)).toBeInTheDocument()
 })
--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,9 @@
-// This is the jest 'setupFilesAfterEnv' setup file
-// It's a good place to set globals, add global before/after hooks, etc
-
+/**
+ * This is the jest 'setupFilesAfterEnv' setup file
+ * It's a good place to set globals, add global before/after hooks, etc
+ */
 import matchers from "@testing-library/jest-dom/matchers"
+import { cleanup } from "@testing-library/react"
 import { expect, vi } from "vitest"
 
 // prevent error: NextRouter was not mounted.
@@ -13,6 +15,10 @@ useRouter.mockImplementation(() => ({
 // prevent error: Invalid Chai property: toBeInTheDocument
 // https://marabesi.com/2023/01/14/configuring-vitest-and-testing-library.html
 expect.extend(matchers)
+
+afterEach(() => {
+  cleanup()
+})
 
 // so TS doesn't complain
 export {}
