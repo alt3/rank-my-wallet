9dafa51ba02a8df5842051a28f49da7e1b471f13
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-09-01 11:12:01 +0200

    Monthly package bumps

--- test/setup.ts
+++ test/setup.ts
@@ -2,19 +2,15 @@
  * This is the jest 'setupFilesAfterEnv' setup file
  * It's a good place to set globals, add global before/after hooks, etc
  */
-import matchers from "@testing-library/jest-dom/matchers"
+import "@testing-library/jest-dom/vitest"
 import { cleanup } from "@testing-library/react"
-import { expect, vi } from "vitest"
+import { vi } from "vitest"
 
 // prevent error: NextRouter was not mounted.
 const useRouter = vi.spyOn(require("next/router"), "useRouter")
 useRouter.mockImplementation(() => ({
   pathname: "/",
 }))
-
-// prevent error: Invalid Chai property: toBeInTheDocument
-// https://marabesi.com/2023/01/14/configuring-vitest-and-testing-library.html
-expect.extend(matchers)
 
 afterEach(() => {
   cleanup()
