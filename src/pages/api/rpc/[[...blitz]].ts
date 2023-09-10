cd308900cb07cc39a19b35c45495f032d1e953c9
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-09-10 12:07:22 +0200

    Bump blitz to 2.0.0-beta.33

--- src/pages/api/rpc/[[...blitz]].ts
+++ src/pages/api/rpc/[[...blitz]].ts
@@ -1,4 +1,8 @@
 import { rpcHandler } from "@blitzjs/rpc"
 import { api } from "src/blitz-server"
 
-export default api(rpcHandler({ onError: console.log }))
+export default api(
+  rpcHandler({
+    onError: console.log,
+  }),
+)
